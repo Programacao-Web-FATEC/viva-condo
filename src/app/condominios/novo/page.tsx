4731c334ae8a3089a442ab33994f37354660d85a
Andrey de S. Almeida <andrey0404.as@gmail.com>
2025-08-22 20:58:43 -0300

    Adicionando tela de condominios

---
+++ src/app/condominios/novo/page.tsx
@@ -0,0 +1,12 @@
+"use client"
+
+export default function CriarCondominio() {
+
+    return (
+        <div id="principal" className="flex items-center justify-center min-h-screen bg-black">
+            <h1 id="name" className="text-2xl font-bold text-center text-white">
+                novo condominio
+            </h1>
+        </div>
+    )
+}
